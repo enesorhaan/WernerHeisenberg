4f8dfaa8bb35d92af6b76a23a9fd9eed78e0348b
enesorhaan <enesorhan_1366@hotmail.com>
2023-09-17 18:12:03 +0300

    Task1 Created

---
+++ Task1/Task1/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        NorthwindContext _db;
+        public EmployeeController(NorthwindContext db)
+        {
+            _db = db;
+        }
+        public IActionResult List()
+        {
+            List<Employee> employees = _db.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(employee);
+            }
+            _db.Employees.Add(employee);
+            _db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ Task1/Task1/Models/FluentValidators/EmployeeValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System.Data;
+
+namespace Task1.Models.FluentValidators
+{
+    public class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().NotNull().WithMessage("This field is required.");
+            RuleFor(x => x.LastName).NotEmpty().NotNull().WithMessage("This field is required.");
+            RuleFor(x => x.Title).NotEmpty().NotNull().WithMessage("This field is required.");
+        }
+    }
+}
